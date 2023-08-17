a7c4850f823f7ff6cb6c2538a9a83885a5eff54d
chain <klaus.mika1052@gmail.com>
2023-08-16 21:26:16 -0300

    fix fetch to return undefined when error

--- src/utils/fetch-audio-from-url.ts
+++ src/utils/fetch-audio-from-url.ts
@@ -1,7 +1,16 @@
-export async function fecthAudioFromUrl(url: string) {
+export async function fetchAudioFromUrl(url: string | undefined) {
+  if (!url) {
+    return
+  }
+
   const file = await fetch(url)
     .then((x) => x.blob())
     .then((blobFile) => new File([blobFile], 'audio', { type: 'audio/wav' }))
+
+  console.log({ file })
+  if (!file) {
+    return undefined
+  }
 
   return file
 }
