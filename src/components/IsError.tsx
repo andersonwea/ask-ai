3eb4917fbb707845994df56c4935999b29766bef
chain <klaus.mika1052@gmail.com>
2023-08-18 17:23:41 -0300

    change component position when appers

--- src/components/IsError.tsx
+++ src/components/IsError.tsx
@@ -1,6 +1,6 @@
 export function IsError() {
   return (
-    <p className="absolute bottom-[230px] font-default text-3xl text-transparent bg-gradient-to-r from-blue-500 to-purple-500 font-extrabold bg-clip-text max-sm:text-2xl">
+    <p className="absolute bottom-[150px] font-default text-3xl text-transparent bg-gradient-to-r from-blue-500 to-purple-500 font-extrabold bg-clip-text max-sm:text-2xl">
       Não foi possível carregar essa musica
     </p>
   )
